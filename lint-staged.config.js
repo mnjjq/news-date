9dfcaddb90f63f056bcd5709ca5dcf271816d7d8
jjq <2510401054@qq.com>
2022-07-11 20:55:47 +0800

    项目初始化-调整初始目录结构

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,3 +1,4 @@
 module.exports = {
   '*.{js,jsx,vue}': 'vue-cli-service lint'
+
 }
--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,15 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
+  <div>
+    <p>123</p>
   </div>
 </template>
 
-<style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+export default {
+
 }
+</script>
 
-nav {
-  padding: 30px;
+<style scoped>
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
